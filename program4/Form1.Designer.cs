52bd26d0e91ade15c850da8dcc9225aec0aa04d9
Tramvajak <kutelvas1@gmail.com>
2022-01-21 17:34:01 +0400

    Добавьте файлы проекта.

---
+++ program4/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace program4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_SaveText = new System.Windows.Forms.Button();
+            this.btn_CreateSignature = new System.Windows.Forms.Button();
+            this.TextBox = new System.Windows.Forms.TextBox();
+            this.text_Sigrature = new System.Windows.Forms.TextBox();
+            this.btn_LoadText = new System.Windows.Forms.Button();
+            this.btn_LoadSignature = new System.Windows.Forms.Button();
+            this.btn_Verify = new System.Windows.Forms.Button();
+            this.btn_SaveSignature = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_SaveText
+            // 
+            this.btn_SaveText.Location = new System.Drawing.Point(38, 12);
+            this.btn_SaveText.Name = "btn_SaveText";
+            this.btn_SaveText.Size = new System.Drawing.Size(75, 23);
+            this.btn_SaveText.TabIndex = 0;
+            this.btn_SaveText.Text = "Save Text";
+            this.btn_SaveText.UseVisualStyleBackColor = true;
+            this.btn_SaveText.Click += new System.EventHandler(this.btn_SaveText_Click);
+            // 
+            // btn_CreateSignature
+            // 
+            this.btn_CreateSignature.Location = new System.Drawing.Point(389, 70);
+            this.btn_CreateSignature.Name = "btn_CreateSignature";
+            this.btn_CreateSignature.Size = new System.Drawing.Size(94, 23);
+            this.btn_CreateSignature.TabIndex = 1;
+            this.btn_CreateSignature.Text = "Create Signature";
+            this.btn_CreateSignature.UseVisualStyleBackColor = true;
+            this.btn_CreateSignature.Click += new System.EventHandler(this.btn_CreateSignature_Click);
+            // 
+            // TextBox
+            // 
+            this.TextBox.Location = new System.Drawing.Point(145, 14);
+            this.TextBox.Multiline = true;
+            this.TextBox.Name = "TextBox";
+            this.TextBox.Size = new System.Drawing.Size(605, 50);
+            this.TextBox.TabIndex = 2;
+            // 
+            // text_Sigrature
+            // 
+            this.text_Sigrature.Location = new System.Drawing.Point(145, 99);
+            this.text_Sigrature.Multiline = true;
+            this.text_Sigrature.Name = "text_Sigrature";
+            this.text_Sigrature.ReadOnly = true;
+            this.text_Sigrature.Size = new System.Drawing.Size(605, 49);
+            this.text_Sigrature.TabIndex = 3;
+            // 
+            // btn_LoadText
+            // 
+            this.btn_LoadText.Location = new System.Drawing.Point(38, 41);
+            this.btn_LoadText.Name = "btn_LoadText";
+            this.btn_LoadText.Size = new System.Drawing.Size(75, 23);
+            this.btn_LoadText.TabIndex = 4;
+            this.btn_LoadText.Text = "Load Text";
+            this.btn_LoadText.UseVisualStyleBackColor = true;
+            this.btn_LoadText.Click += new System.EventHandler(this.btn_LoadText_Click);
+            // 
+            // btn_LoadSignature
+            // 
+            this.btn_LoadSignature.Location = new System.Drawing.Point(29, 125);
+            this.btn_LoadSignature.Name = "btn_LoadSignature";
+            this.btn_LoadSignature.Size = new System.Drawing.Size(94, 23);
+            this.btn_LoadSignature.TabIndex = 5;
+            this.btn_LoadSignature.Text = "Load Signature";
+            this.btn_LoadSignature.UseVisualStyleBackColor = true;
+            this.btn_LoadSignature.Click += new System.EventHandler(this.btn_LoadSignature_Click);
+            // 
+            // btn_Verify
+            // 
+            this.btn_Verify.Location = new System.Drawing.Point(376, 154);
+            this.btn_Verify.Name = "btn_Verify";
+            this.btn_Verify.Size = new System.Drawing.Size(120, 23);
+            this.btn_Verify.TabIndex = 6;
+            this.btn_Verify.Text = "Verify";
+            this.btn_Verify.UseVisualStyleBackColor = true;
+            this.btn_Verify.Click += new System.EventHandler(this.btn_Verify_Click);
+            // 
+            // btn_SaveSignature
+            // 
+            this.btn_SaveSignature.Location = new System.Drawing.Point(29, 96);
+            this.btn_SaveSignature.Name = "btn_SaveSignature";
+            this.btn_SaveSignature.Size = new System.Drawing.Size(94, 23);
+            this.btn_SaveSignature.TabIndex = 7;
+            this.btn_SaveSignature.Text = "Save Signature";
+            this.btn_SaveSignature.UseVisualStyleBackColor = true;
+            this.btn_SaveSignature.Click += new System.EventHandler(this.btn_SaveSignature_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 209);
+            this.Controls.Add(this.btn_SaveSignature);
+            this.Controls.Add(this.btn_Verify);
+            this.Controls.Add(this.btn_LoadSignature);
+            this.Controls.Add(this.btn_LoadText);
+            this.Controls.Add(this.text_Sigrature);
+            this.Controls.Add(this.TextBox);
+            this.Controls.Add(this.btn_CreateSignature);
+            this.Controls.Add(this.btn_SaveText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_SaveText;
+        private System.Windows.Forms.Button btn_CreateSignature;
+        private System.Windows.Forms.TextBox TextBox;
+        private System.Windows.Forms.TextBox text_Sigrature;
+        private System.Windows.Forms.Button btn_LoadText;
+        private System.Windows.Forms.Button btn_LoadSignature;
+        private System.Windows.Forms.Button btn_Verify;
+        private System.Windows.Forms.Button btn_SaveSignature;
+    }
+}
+
---
+++ program4/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace program4
+{
+    public partial class Form1 : Form
+    {
+
+        private const string PathFileText = "SimpleText.txt";
+        private const string PathFileSignature = "Signature.dsa";
+        private DSACryptoServiceProvider dsa;
+        private byte[] signaturebytes;
+        private DSAParameters dsaparams;
+        string OID = CryptoConfig.MapNameToOID("SHA1");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btn_SaveText_Click(object sender, EventArgs e) // сохранение теста
+        {
+            using (StreamWriter writer = new StreamWriter(PathFileText))
+            {
+                writer.Write(TextBox.Text);
+            }
+        }
+
+        private void btn_LoadText_Click(object sender, EventArgs e) // загрузка текста
+        {
+            using(StreamReader reader = new StreamReader(PathFileText))
+            {
+                TextBox.Text = reader.ReadToEnd();
+            }
+        }
+        private void btn_CreateSignature_Click(object sender, EventArgs e) // Создание новой сигнатуры
+        {
+            //get original message as byte array
+            byte[] messagebytes = Encoding.UTF8.GetBytes(TextBox.Text);
+
+            //create digest of original message using SHA1
+            SHA1 sha1 = new SHA1CryptoServiceProvider();
+            byte[] hashbytes = sha1.ComputeHash(messagebytes);
+
+
+            //create DSA object using default key
+            dsa = new DSACryptoServiceProvider();
+
+            //sign hash using OID for SHA-1
+            signaturebytes = dsa.SignHash(hashbytes, "1.3.14.3.2.26");
+            DebugHash(signaturebytes);
+
+            //provide DSA parameters to verification
+            dsaparams = dsa.ExportParameters(false);
+
+            Debug.WriteLine(dsa.ExportParameters(false).P);
+
+            //display digital signature in hex format
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < signaturebytes.Length; i++)
+                sb.Append(String.Format("{0,2:X2} ", signaturebytes[i]));
+            text_Sigrature.Text = sb.ToString();
+        }
+        /// <summary>
+        /// Выводит сигнатуру в техт бокс после загрузки из файла
+        /// </summary>
+        private void TextBoxSingHash()
+        {
+            byte[] messagebytes = Encoding.UTF8.GetBytes(TextBox.Text);
+
+            //create digest of original message using SHA1
+            SHA1 sha1 = new SHA1CryptoServiceProvider();
+            byte[] hashbytes = sha1.ComputeHash(messagebytes);
+
+            signaturebytes = dsa.CreateSignature(hashbytes);
+
+            DebugHash(signaturebytes);
+            //if(dsa.VerifyHash(hashbytes,""))
+
+           // display digital signature in hex format
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < signaturebytes.Length; i++)
+                sb.Append(String.Format("{0,2:X2} ", signaturebytes[i]));
+            text_Sigrature.Text = sb.ToString();
+        }
+        private void btn_SaveSignature_Click(object sender, EventArgs e) // сохранение открытого и закрытого ключа
+        {
+            using (StreamWriter writer = new StreamWriter(PathFileSignature))
+                writer.Write(dsa.ToXmlString(true));
+
+        }
+        private void btn_LoadSignature_Click(object sender, EventArgs e) // загрузка открытого и закрытого ключа
+        {
+            string key = null;
+            using (StreamReader reader = new StreamReader(PathFileSignature))
+                key = reader.ReadToEnd();
+
+            dsa = new DSACryptoServiceProvider();
+
+            dsa.FromXmlString(key);
+
+            TextBoxSingHash();
+           
+        }
+
+        private void btn_Verify_Click(object sender, EventArgs e) // проверка целостности файла
+        {
+            byte[] messagebytes = Encoding.UTF8.GetBytes(TextBox.Text);
+
+            //create digest of original message using SHA1
+            SHA1 sha1 = new SHA1CryptoServiceProvider();
+            byte[] hashbytes = sha1.ComputeHash(messagebytes);
+
+            //do verification on hash using OID for SHA - 1
+            DebugHash(signaturebytes);
+            bool match = dsa.VerifyHash(hashbytes, "1.3.14.3.2.26", signaturebytes);
+
+            //show message box with result of verification
+            String strResult;
+            if (match)
+                strResult = "VerifySignature returned TRUE";
+            else
+                strResult = "VerifySignature returned FALSE";
+            MessageBox.Show(
+                strResult,
+                "Result From Calling VerifySignature",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Exclamation);
+        }
+        /// <summary>
+        /// Debug Write Line array Hash
+        /// </summary>
+        /// <param name="hash">Hash</param>
+        private void DebugHash(byte[] hash)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+                sb.Append(String.Format("{0,2:X2} ", hash[i]));
+            Debug.WriteLine(sb.ToString());
+        }
+    }
+}
